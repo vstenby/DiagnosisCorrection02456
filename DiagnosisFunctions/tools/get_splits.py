0470f2a63618dc16dc9f062e2c7f12bb06ada71d
Viktor Stenby Johansson <s174483@student.dtu.dk>
2021-11-01 15:00:27 +0100

    Added splits

--- DiagnosisFunctions/tools/get_splits.py
+++ DiagnosisFunctions/tools/get_splits.py
@@ -2,22 +2,31 @@ import os
 import numpy as np
 from sklearn.model_selection import train_test_split
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 def get_splits(random_state = 42, data_folder = './data/', file_extension = '.jpeg'):
     
     images_path = os.path.join(data_folder, 'images')
     labels_path = os.path.join(data_folder, 'dermx_labels.csv')
     
-    #Get all images
-    images = np.array([os.path.join(images_path, x) for x in os.listdir(images_path) if x.endswith('.jpeg')])
-    
-    #Load the labels
     labels = pd.read_csv(labels_path)
+
+    labels['path']   = labels['image_id'].apply(lambda x : os.path.join(images_path, f'{x}.jpeg'))
+    labels['exists'] = labels['path'].apply(lambda x : os.path.exists(x)).astype(int)
+
+    #Drop the rows we don't have data on.
+    labels = labels.loc[labels['exists'] == 1]
+    
+    le = LabelEncoder()
+    
+    #To begin with, we just want the path and the diagnosis.
+    data = pd.DataFrame({'path' : labels['path'], 'target' : le.fit_transform(labels['diagnosis'])})
+    
     
     #Train is 70%, val is 15% and test is 15%. 
-    train, val = train_test_split(labels, test_size = 0.3, random_state = random_state)
+    train, val = train_test_split(data, test_size = 0.3, random_state = random_state)
     val, test  = train_test_split(val, test_size = 0.5, random_state = random_state)
     
     
     
-    return train, val, test
+    return (train.path.tolist(), train.target.tolist()), (val.path.tolist(),   val.target.tolist()), (test.path.tolist(),  test.target.tolist()), le
