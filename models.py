1693baea840a20443d964e6ec59fd34373abe8fa
Viktor Stenby Johansson <s174483@student.dtu.dk>
2021-11-29 13:13:58 +0100

    Updated models to use efficientnet

--- models.py
+++ models.py
@@ -8,14 +8,18 @@ class CNN1(nn.Module):
     def __init__(self, n_diagnosis = 6):
         super().__init__()
     
-        #We can still fine tune it.
-        self.base_model = models.resnext50_32x4d(pretrained=True)
-
-        self.fc_diagnosis       = nn.Linear(in_features = self.base_model.fc.out_features, out_features = n_diagnosis)
+        #Take the efficientnet_b0 
+        model = models.efficientnet_b0(pretrained=True)
+        num_features        = model.classifier[1].in_features
+        model.classifier[1] = nn.Linear(num_features, 1024) #Replace the last layer.
+        self.base_model     = model 
+                          
+        self.fc_diagnosis = nn.Linear(in_features = 1024, out_features = n_diagnosis)
 
     def forward(self, x):
         #Pass the image through resnet. It's still being trained.
         x = self.base_model(x) 
+        x = torch.nn.ReLU(x)
 
         #Diagnosis pipeline
         x_diagnosis = self.fc_diagnosis(x)
@@ -27,16 +31,20 @@ class CNN2(nn.Module):
     def __init__(self, n_characteristics = 7, n_diagnosis = 6, n_area = 4):
         super().__init__()
         
-        #We can still fine tune it.
-        self.base_model = models.resnext50_32x4d(pretrained=True)
+        #Take the efficientnet_b0 
+        model = models.efficientnet_b0(pretrained=True)
+        num_features        = model.classifier[1].in_features
+        model.classifier[1] = nn.Linear(num_features, 1024) #Replace the last layer.
+        self.base_model     = model 
 
-        self.fc_characteristics = nn.Linear(in_features = self.base_model.fc.out_features, out_features = n_characteristics)
-        self.fc_diagnosis       = nn.Linear(in_features = self.base_model.fc.out_features, out_features = n_diagnosis)
-        self.fc_area            = nn.Linear(in_features = self.base_model.fc.out_features, out_features = n_area)
+        self.fc_characteristics = nn.Linear(in_features = 1024, out_features = n_characteristics)
+        self.fc_diagnosis       = nn.Linear(in_features = 1024, out_features = n_diagnosis)
+        self.fc_area            = nn.Linear(in_features = 1024, out_features = n_area)
 
     def forward(self, x):
         #Pass the image through resnet. It's still being trained.
         x = self.base_model(x) 
+        x = torch.nn.ReLU(x)
 
         #Characteristics pipeline
         x_characteristics = self.fc_characteristics(x)
@@ -51,5 +59,5 @@ class CNN2(nn.Module):
         x_area = torch.softmax(x_area, dim=-1)
 
         x_cat  = torch.cat((x_characteristics, x_diagnosis, x_area), dim=1)
-        
+                
         return x_cat
