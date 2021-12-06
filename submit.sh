90d8c17901adbae5d306b4936986ba3a959b8cbb
Rasmus Aagaard <raagaard97@gmail.com>
2021-12-06 21:21:17 +0100

    optuna

---
+++ submit.sh
@@ -0,0 +1,40 @@
+#!/bin/sh 
+### General options 
+### -- specify queue -- 
+#BSUB -q gpuv100
+### -- set the job Name -- 
+#BSUB -J DiagnosisCorrection
+### -- ask for number of cores (default: 1) -- 
+#BSUB -n 1
+### -- Select the resources: 1 gpu in exclusive process mode --
+#BSUB -gpu "num=1:mode=exclusive_process"
+### -- specify that the cores must be on the same host -- 
+#BSUB -R "span[hosts=1]"
+### -- specify that we need 5GB of memory per core/slot -- 
+#BSUB -R "rusage[mem=5GB]"
+#BSUB -R "select[gpu32gb]"
+### -- set walltime limit: hh:mm -- 
+#BSUB -W 24:00 
+### -- set the email address -- 
+# please uncomment the following line and put in your e-mail address,
+# if you want to receive e-mail notifications on a non-default address
+#BSUB -u s164419@student.dtu.dk
+### -- send notification at start -- 
+#BSUB -B 
+### -- send notification at completion -- 
+#BSUB -N 
+### -- Specify the output and error file. %J is the job-id -- 
+### -- -o and -e mean append, -oo and -eo mean overwrite -- 
+#BSUB -o Output_%J.out 
+#BSUB -e Error_%J.err
+
+# here follow the commands you want to execute
+
+/appl/cuda/10.2/samples/NVIDIA_CUDA-10.2_Samples/bin/x86_64/linux/release/deviceQuery
+
+nvidia-smi
+module load cuda/10.2
+
+source /zhome/55/6/117776/repos/DiagnosisCorrection02456/venv/bin/activate
+ipython -c "%run /zhome/55/6/117776/repos/DiagnosisCorrection02456/CharacteristicsTrain.ipynb"
+
