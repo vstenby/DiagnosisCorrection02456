26c5495acfbc4a0aa7ffdb5ea5c88b8e78aab897
Rasmus Aagaard <raagaard97@gmail.com>
2021-12-08 13:05:02 +0100

    repo refactoring

--- CNNmodels.py
+++ CNNmodels.py
@@ -4,38 +4,15 @@ import torch.optim as optim
 import torch.nn as nn
 import torchvision.models as models
 
-class CNN1(nn.Module):
-    def __init__(self, n_diagnosis = 6):
-        super().__init__()
-    
-        #Take the efficientnet_b0 
-        model = models.efficientnet_b0(pretrained=True)
-        num_features        = model.classifier[1].in_features
-        model.classifier[1] = nn.Linear(num_features, 1024) #Replace the last layer.
-        self.base_model     = model 
-                          
-        self.fc_diagnosis = nn.Linear(in_features = 1024, out_features = n_diagnosis)
-
-    def forward(self, x):
-        #Pass the image through resnet. It's still being trained.
-        x = self.base_model(x) 
-        x = torch.nn.ReLU()(x)
-
-        #Diagnosis pipeline
-        x_diagnosis = self.fc_diagnosis(x)
-        x_diagnosis = torch.softmax(x_diagnosis, dim=-1) #Since it should sum to 1.
-        
-        return x_diagnosis
-
-class CNN2(nn.Module):
+class CNN(nn.Module):
     def __init__(self, n_characteristics = 7, n_diagnosis = 6, n_area = 4):
         super().__init__()
         
         #Take the efficientnet_b0 
-        model = models.efficientnet_b0(pretrained=True)
-        num_features        = model.classifier[1].in_features
-        model.classifier[1] = nn.Linear(num_features, 1024) #Replace the last layer.
-        self.base_model     = model 
+        model = models.googlenet(pretrained=True)
+        num_features = model.fc.in_features
+        model.fc = nn.Linear(num_features, 1024) #Replace the last layer.
+        self.base_model = model 
 
         self.fc_characteristics = nn.Linear(in_features = 1024, out_features = n_characteristics)
         self.fc_diagnosis       = nn.Linear(in_features = 1024, out_features = n_diagnosis)
